896fe9a7f2c2c594ef4ee2efed6702cd856b04c9
Pin Jui <fee8ack21@gmail.com>
2021-04-27 11:31:11 +0800

    feature finished

--- js/all.js
+++ js/all.js
@@ -1,7 +1,9 @@
 const allBtn = document.getElementById("all-btn");
 const activedBtn = document.getElementById("actived-btn");
 const finishedBtn = document.getElementById("finished-btn");
-
+// prepare render condition
+let condition = "all";
+// category button style change
 allBtn.addEventListener("click", function () {
   if (this.classList.contains("btn-primary")) {
     return;
@@ -9,7 +11,8 @@ allBtn.addEventListener("click", function () {
     this.classList.add("btn-primary");
     activedBtn.classList.remove("btn-primary");
     finishedBtn.classList.remove("btn-primary");
-    render("all");
+    condition = "all";
+    render(condition);
   }
 });
 activedBtn.addEventListener("click", function () {
@@ -19,7 +22,8 @@ activedBtn.addEventListener("click", function () {
     this.classList.add("btn-primary");
     allBtn.classList.remove("btn-primary");
     finishedBtn.classList.remove("btn-primary");
-    render("actived");
+    condition = "actived";
+    render(condition);
   }
 });
 finishedBtn.addEventListener("click", function () {
@@ -29,10 +33,12 @@ finishedBtn.addEventListener("click", function () {
     this.classList.add("btn-primary");
     activedBtn.classList.remove("btn-primary");
     allBtn.classList.remove("btn-primary");
-    render("finished");
+    condition = "finished";
+    render(condition);
   }
 });
-//
+
+// todos list reducer
 function todoApp(
   state = localStorage.getItem("redux_todos")
     ? JSON.parse(localStorage.getItem("redux_todos"))
@@ -60,6 +66,9 @@ function todoApp(
           completed: action["completed"],
         },
       ];
+    //
+    //
+    //
     case "TODO_COMPLETE":
       localStorage.setItem(
         "redux_todos",
@@ -82,6 +91,9 @@ function todoApp(
           })
         )
         .concat([...state].slice(action.index + 1));
+    //
+    //
+    //
     case "TODO_REMOVE":
       if (action.index === 0) {
         localStorage.setItem(
@@ -101,13 +113,43 @@ function todoApp(
       return [...state]
         .slice(0, action.index)
         .concat([...state].slice(action.index + 1));
+    //
+    //
+    //
+    case "TODO_MOD":
+      localStorage.setItem(
+        "redux_todos",
+        JSON.stringify(
+          [...state]
+            .slice(0, action.index)
+            .concat(
+              Object.assign({}, [...state][action.index], {
+                text: action.text,
+              })
+            )
+            .concat([...state].slice(action.index + 1))
+        )
+      );
+      return [...state]
+        .slice(0, action.index)
+        .concat(
+          Object.assign({}, [...state][action.index], {
+            text: action.text,
+          })
+        )
+        .concat([...state].slice(action.index + 1));
+    //
+    //
+    //
     default:
       localStorage.setItem("redux_todos", JSON.stringify(state));
       return state;
   }
 }
+// init store
 const store = Redux.createStore(todoApp);
-//
+
+// add function & action
 function add() {
   const text = document.getElementById("add-input").value.trim();
   if (text === "") {
@@ -122,127 +164,133 @@ function add() {
   document.getElementById("add-input").value = "";
   document.getElementById("add-input").focus();
 }
+
+// toggle function & action
 function toggle(index) {
-  // console.log('toggle function')
   store.dispatch({ type: "TODO_COMPLETE", index: index });
 }
+
+// remove function & action
 function remove(index) {
-  // console.log('remove function')
   store.dispatch({ type: "TODO_REMOVE", index: index });
 }
-function render(condition = "all") {
+//
+function mod(index, text) {
+  store.dispatch({ type: "TODO_MOD", index: index, text: text });
+}
+// todo component
+function TodoComponent(props) {
+  const { useState, useRef } = React;
+  const textInput = useRef(null);
+
+  const [inputState, setInputState] = useState(false);
+  return (
+    <li
+      className={`d-flex justify-content-between align-items-center py-2 px-3 mb-2 rounded w-100 ${
+        props.item.completed ? "bg-dark" : "bg-light"
+      }`}
+      key={props.item.time}
+      onClick={() => {
+        toggle(props.index);
+      }}
+    >
+      <input
+        className={`form-control mb-0 
+        ${props.item.completed ? " actived " : ""} 
+        ${
+          inputState
+            ? " bg-white bd-color-actived "
+            : " bg-transparent bd-color-none "
+        }
+        ${props.item.completed && !inputState ? " text-white " : ""}
+        `}
+        style={{ width: "calc(100% - 104.41px)" }}
+        disabled={inputState ? "" : "disabled"}
+        ref={textInput}
+        defaultValue={props.item.text}
+        onChange={(e) => {
+          if (e.target.value.trim().length === 0) {
+            e.target.value = "Can not be empty!";
+          }
+          mod(props.index, e.target.value.trim());
+        }}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      />
+      <div>
+        <button
+          className="btn btn-warning"
+          onClick={async (e) => {
+            e.stopPropagation();
+            await setInputState(!inputState);
+            textInput.current.focus();
+          }}
+        >
+          <i className="fas fa-sliders-h"></i>
+        </button>
+        <button
+          className="btn btn-danger ml-2"
+          onClick={(e) => {
+            e.stopPropagation();
+            remove(props.index);
+          }}
+        >
+          <i className="fas fa-trash-alt"></i>
+        </button>
+      </div>
+    </li>
+  );
+}
+//
+function render(condition) {
   //
   if (condition === "all") {
     if (store.getState().length > 0) {
-      const jsxStr = store.getState().map((item, index) => (
-        <li
-          className={`d-flex justify-content-between align-items-center py-2 px-3 mb-2 rounded w-100 ${
-            item.completed ? "bg-secondary" : "bg-light"
-          }`}
-          key={item.time}
-          onClick={() => {
-            toggle(index);
-          }}
-        >
-          <p className={`mb-0 ${item.completed ? "text-white actived" : ""}`}>
-            {item.text}
-          </p>
-          <div>
-            <button
-              className="btn btn-danger ml-2"
-              onClick={(e) => {
-                e.stopPropagation();
-                remove(index);
-              }}
-            >
-              <i className="fas fa-trash-alt"></i>
-            </button>
-          </div>
-        </li>
-      ));
+      const jsxStr = store
+        .getState()
+        .map((item, index) => (
+          <TodoComponent key={index} item={item} index={index} />
+        ));
       //
       ReactDOM.render(jsxStr, document.getElementById("list"));
     } else {
-      const jsxStr = <></>;
-      ReactDOM.render(jsxStr, document.getElementById("list"));
+      ReactDOM.render(<></>, document.getElementById("list"));
     }
   } else if (condition === "actived") {
     if (store.getState().length > 0) {
-      const jsxStr = store.getState().map(
-        (item, index) =>
-          !item.completed && (
-            <li
-              className={`d-flex justify-content-between align-items-center py-2 px-3 mb-2 rounded w-100 ${
-                item.completed ? "bg-secondary" : "bg-light"
-              }`}
-              key={item.time}
-              onClick={() => {
-                toggle(index);
-              }}
-            >
-              <p
-                className={`mb-0 ${item.completed ? "text-white actived" : ""}`}
-              >
-                {item.text}
-              </p>
-              <div>
-                <button
-                  className="btn btn-danger ml-2"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    remove(index);
-                  }}
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-              </div>
-            </li>
-          )
-      );
+      const jsxStr = store
+        .getState()
+        .map(
+          (item, index) =>
+            !item.completed && (
+              <TodoComponent key={index} item={item} index={index} />
+            )
+        );
       ReactDOM.render(jsxStr, document.getElementById("list"));
     } else {
-      const jsxStr = <></>;
-      ReactDOM.render(jsxStr, document.getElementById("list"));
+      ReactDOM.render(<></>, document.getElementById("list"));
     }
   } else if (condition === "finished") {
     if (store.getState().length > 0) {
-      const jsxStr = store.getState().map(
-        (item, index) =>
-          item.completed && (
-            <li
-              className={`d-flex justify-content-between align-items-center py-2 px-3 mb-2 rounded w-100 ${
-                item.completed ? "bg-secondary" : "bg-light"
-              }`}
-              key={item.time}
-              onClick={() => {
-                toggle(index);
-              }}
-            >
-              <p
-                className={`mb-0 ${item.completed ? "text-white actived" : ""}`}
-              >
-                {item.text}
-              </p>
-              <div>
-                <button
-                  className="btn btn-danger ml-2"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    remove(index);
-                  }}
-                >
-                  <i className="fas fa-trash-alt"></i>
-                </button>
-              </div>
-            </li>
-          )
-      );
+      const jsxStr = store
+        .getState()
+        .map(
+          (item, index) =>
+            item.completed && (
+              <TodoComponent key={index} item={item} index={index} />
+            )
+        );
       ReactDOM.render(jsxStr, document.getElementById("list"));
     } else {
-      const jsxStr = <></>;
-      ReactDOM.render(jsxStr, document.getElementById("list"));
+      ReactDOM.render(<></>, document.getElementById("list"));
     }
   }
 }
-render();
-store.subscribe(render);
+// init render
+render(condition);
+
+// listen dispatch then render
+store.subscribe(() => {
+  render(condition);
+});
